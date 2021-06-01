e993ea29ea9eb196dcb4015a4cefca12958405c7
Alaroudi <alaroudi.abdel@gmail.com>
2021-05-31 19:15:46 -0500

    Add Delete route method

--- index.js
+++ index.js
@@ -70,5 +70,18 @@ app.put("/api/genres/:id", (req, res) => {
   res.send(genre);
 });
 
+// Delete request handlers
+app.delete("/api/genres/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+
+  const genre = genres.find((element) => element.id === id);
+  if (!genre) {
+    return res.status(404).send("Genre with given id is not found.");
+  }
+
+  genres.splice(genres.indexOf(genre), 1);
+  res.send(genre);
+});
+
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Listening to port ${port}`));
